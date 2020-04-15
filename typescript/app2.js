bfa1882776d51813f21c9fc8f1231ed0f19cd630
= <pamela.bellaguerra@gmail.com>
2020-04-14 21:39:25 -0300

    Criação da Classe Carro para estudo

---
+++ typescript/app2.js
@@ -0,0 +1,21 @@
+var Carro = /** @class */ (function () {
+    function Carro() {
+    }
+    return Carro;
+}());
+-0;
+acelerar();
+void {
+    "this": .velocidade = this.velocidade + 10
+};
+parar();
+void {
+    "this": .velocidade = 0
+};
+velocidadeAtual();
+number;
+{
+    return this.velocidade;
+}
+var carroA = new Carro();
+console.log(carroA);
